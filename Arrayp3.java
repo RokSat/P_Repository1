75d0cf1c14a45bd3b9fc790e525386e92a4ab035
RokSat <126390700+RokSat@users.noreply.github.com>
2023-02-27 15:40:29 +0530

    Add files via upload

---
+++ Arrayp3.java
@@ -0,0 +1,21 @@
+// addition of Array
+
+class ArrayP3
+{
+
+public static void main(String[] args)
+{
+
+int arr1[]={1,3,5,7};
+int arr2[]={2,4,6,8};
+
+int arr3[]=arr1[]+arr2[];
+
+ System.out.println(arr3[]);
+
+
+
+}
+
+
+}
